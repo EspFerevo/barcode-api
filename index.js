252b80651d8fe53dc99048cc9c7471eaade77d00
Oleksandr Chekrenov <olekchekrenov@gmail.com>
2025-05-08 12:45:19 +0300

    1

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const bwipjs = require('bwip-js');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const express = require("express");
+const bwipjs = require("bwip-js");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const app = express();
 const port = 3000;
 
 // Swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Статика (HTML, CSS, JS)
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Генерация штрих-кода
-app.get('/generate-barcode', async (req, res) => {
-  const { data = '123456789012', type = 'code128' } = req.query;
+app.get("/generate-barcode", async (req, res) => {
+  const { data = "123456789012", type = "code128" } = req.query;
 
   try {
     const svg = await bwipjs.toBuffer({
@@ -23,19 +23,18 @@ app.get('/generate-barcode', async (req, res) => {
       scale: 3,
       height: 10,
       includetext: true,
-      output: 'svg' // <-- ВОТ ЭТО КЛЮЧЕВОЕ
+      output: "svg", // Важно: генерируем SVG
     });
 
-    res.set('Content-Type', 'image/svg+xml');
+    res.set("Content-Type", "image/svg+xml");
     res.send(svg);
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 });
 
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(port, () => {
