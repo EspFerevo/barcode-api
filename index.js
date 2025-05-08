d763927b4e1dc6c58822818e46d822a0a29826ce
Oleksandr Chekrenov <olekchekrenov@gmail.com>
2025-05-08 12:20:30 +0300

    Update

--- index.js
+++ index.js
@@ -1,36 +1,42 @@
-const express = require("express");
-const bwipjs = require("bwip-js");
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./swagger.json");
+const express = require('express');
+const bwipjs = require('bwip-js');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 const port = 3000;
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get("/generate-barcode", async (req, res) => {
-  const { data = "123456789012", type = "code128" } = req.query;
+// Статика (HTML, CSS, JS)
+app.use(express.static('public'));
+
+// Генерация штрих-кода
+app.get('/generate-barcode', async (req, res) => {
+  const { data = '123456789012', type = 'code128' } = req.query;
 
   try {
     const png = await bwipjs.toBuffer({
-      bcid: type, // Тип штрих-кода: code128, ean13, upca, и т.д.
+      bcid: type,
       text: data,
       scale: 3,
       height: 10,
-      includetext: true,
+      includetext: true
     });
 
-    res.set("Content-Type", "image/png");
+    res.set('Content-Type', 'image/png');
     res.send(png);
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 });
 
-app.get("/", (req, res) => {
-  res.send("Barcode API is running. Visit /api-docs for Swagger UI.");
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(port, () => {
-  console.log(`🚀 Server running at http://localhost:${port}`);
+  console.log(`🚀 Сервер работает на http://localhost:${port}`);
+  console.log(`🔎 Swagger UI: http://localhost:${port}/api-docs`);
 });
